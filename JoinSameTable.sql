f8a5d7902d62fc83269a4a170715d44eef0bfd2f
Abdelrahman Yehia <54885581+A-lang209@users.noreply.github.com>
2024-09-09 19:43:13 +0300

    Update JoinSameTable.sql

--- JoinSameTable.sql
+++ JoinSameTable.sql
@@ -1,4 +1,4 @@
 SELECT chars.name AS character_name, mentors.name AS mentor_name
 FROM fanatsy.characters chars
-JOIN fanatsy.characters mentors
+INNER JOIN fanatsy.characters mentors
 ON chars.mentor_id = mentors.id
